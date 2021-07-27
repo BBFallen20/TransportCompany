ed782cc6903d61d8bd9c3dc364bef314d06e8d14
BBFallen20 <blackbloodfallen123@gmail.com>
2021-07-27 11:56:33 +0300

    Initial commit. Created Vehicle, Driver, DrivingLicense and VehicleDriver tables with relations. DEBUG setting configured by environ variables. Installed Django REST framework.

---
+++ CarDelivery/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Vehicle, Driver, DrivingLicense, VehicleDriver
+
+
+class VehicleDriverInline(admin.StackedInline):
+    model = VehicleDriver
+    fields = ['driver']
+    extra = 1
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    inlines = [VehicleDriverInline]
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DrivingLicense)
+class DrivingLicenseAdmin(admin.ModelAdmin):
+    pass
+
+
+
---
+++ CarDelivery/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+class Vehicle(models.Model):
+    model = models.CharField(max_length=100, verbose_name='Vehicle model title')
+    mark = models.CharField(max_length=100, verbose_name='Vehicle mark title')
+    driving_category = models.ForeignKey(
+        'DrivingLicense',
+        on_delete=models.CASCADE,
+        verbose_name='Required driving category'
+    )
+    using = models.BooleanField(default=False, editable=False, verbose_name='Vehicle in use flag')
+    drivers_limit = models.PositiveSmallIntegerField(verbose_name='Vehicle drivers limit', default=1)
+
+    def __str__(self) -> str:
+        return f"Car#{self.id} - {self.mark} {self.model}."
+
+    def __repr__(self):
+        return f"Car({self.id} - {self.mark} {self.model})"
+
+
+class VehicleDriver(models.Model):
+    vehicle = models.ForeignKey(
+        'Vehicle',
+        on_delete=models.CASCADE,
+        verbose_name='Vehicle',
+        related_query_name='vehicle_vehicle_driver'
+    )
+    driver = models.ForeignKey(
+        'Driver',
+        on_delete=models.CASCADE,
+        verbose_name='Driver',
+        related_query_name='driver_vehicle_driver'
+    )
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='Driver firstname')
+    last_name = models.CharField(max_length=100, verbose_name='Driver lastname')
+    driving_license = models.ManyToManyField('DrivingLicense', verbose_name='Driving license')
+
+    def __str__(self) -> str:
+        return f"Driver#{self.id} {self.first_name} {self.last_name}."
+
+    def __repr__(self) -> str:
+        return f"Driver({self.first_name} {self.last_name})"
+
+    class Meta:
+        verbose_name = 'Driver'
+        verbose_name_plural = 'Drivers'
+
+
+class DrivingLicense(models.Model):
+    title = models.CharField(max_length=10, verbose_name='License title')
+
+    def __str__(self) -> str:
+        return f"License {self.title}"
+
+    def __repr__(self) -> str:
+        return f"License({self.id} {self.title})"
+
+    class Meta:
+        verbose_name = 'Driving license'
+        verbose_name_plural = 'Driving licenses'
