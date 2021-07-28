da2a80e2f3237c895ca5fe185042cd5c2b7f9aea
BBFallen20 <blackbloodfallen123@gmail.com>
2021-07-28 09:43:53 +0300

    services.py methods naming remake.

--- car_delivery/services.py
+++ car_delivery/services.py
@@ -6,19 +6,19 @@ class VehicleDriverCreationValidator:
         self.vehicle = vehicle
         self.driver = driver
 
-    def check_vehicle_drivers_limit_availability(self):
+    def check_limit_availability(self):
         if self.vehicle.drivers_limit <= self.vehicle.get_drivers_count():
             raise serializers.ValidationError("This car has reached max drivers limit.")
 
-    def check_vehicle_driver_already_set(self):
+    def check_already_set(self):
         if self.driver in [vehicle_driver.driver for vehicle_driver in self.vehicle.vehicledriver_set.all()]:
             raise serializers.ValidationError("This driver already assigned to this vehicle.")
 
-    def check_vehicle_driver_driving_category(self):
+    def check_driving_category(self):
         if self.vehicle.driving_category not in self.driver.get_license_list():
             raise serializers.ValidationError("Driver has not required driving license.")
 
     def check_all(self):
-        self.check_vehicle_driver_driving_category()
-        self.check_vehicle_driver_already_set()
-        self.check_vehicle_drivers_limit_availability()
+        self.check_limit_availability()
+        self.check_already_set()
+        self.check_driving_category()
