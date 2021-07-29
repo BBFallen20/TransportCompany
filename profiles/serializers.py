4b46e7abf0f95c369b26c88179f50fe4254e5336
BBFallen20 <blackbloodfallen123@gmail.com>
2021-07-29 16:16:38 +0300

    Register serializer extended for user role.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.db import transaction
+from rest_auth.registration.serializers import RegisterSerializer
 from .models import User
 
 
@@ -6,3 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    role = serializers.ChoiceField(choices=User.RoleChoice.choices)
+
+    @transaction.atomic
+    def save(self, request):
+        user = super().save(request)
+        user.role = self.data.get('role')
+        user.save()
+        return user
