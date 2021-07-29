7fd1324e1191d4d61fb95bf25c657f449e8c042a
BBFallen20 <blackbloodfallen123@gmail.com>
2021-07-29 16:28:32 +0300

    master branch DB settings fix.

--- transport_company/settings.py
+++ transport_company/settings.py
@@ -87,8 +87,19 @@ WSGI_APPLICATION = 'transport_company.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd887emcfqvs67p',
+
+        'USER': 'ubkpcngtdhzojh',
+
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+
+        'HOST': 'ec2-44-194-225-27.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
 }
 
